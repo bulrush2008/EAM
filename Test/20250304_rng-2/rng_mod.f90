d1098ee522412516c1cd2d951ff5a5475045df0c
shuning@simpop <shnxia@126.com>
2025-03-04 14:23:40 +0800

    comments updated

--- Test/20250304_rng-2/rng_mod.f90
+++ Test/20250304_rng-2/rng_mod.f90
@@ -7,7 +7,7 @@ module rng_mod
   type, public :: RNG
     private
     integer(8)        :: state
-    real              :: spare = 0.0
+    real              :: spare = 0.0  ! 高斯分布需要，均匀分布不需要
     logical           :: has_spare = .false.
   contains
     procedure :: init => seed_rng
@@ -17,7 +17,7 @@ module rng_mod
 
   integer(8), parameter :: LCG_MULT = 6364136223846793005_8
   integer(8), parameter :: LCG_INCR = 1442695040888963407_8
-  integer(8), parameter :: MODULUS = 9223372036854775807_8  ! 2^63-1
+  integer(8), parameter :: MODULUS  = 9223372036854775807_8  ! 2^63-1
 
 contains
 
@@ -34,6 +34,7 @@ contains
     integer(8) :: next_state
     
     ! 更新状态并生成均匀分布随机数
+    ! 注意，mod(x,y) 中 x 可能由于过大，被机器自动识别为负数，因此需要 abs()
     next_state = mod(LCG_MULT * this%state + LCG_INCR, MODULUS)
     u = real(next_state) / real(MODULUS)  ! [0,1) 范围
     u = abs(u)
